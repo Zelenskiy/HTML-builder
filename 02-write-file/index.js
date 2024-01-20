5e54e4157fbaba367606bc056615d8f064b1e17c
Zelenskiyas <zelensk1973@gmail.com>
2024-01-20 15:28:25 +0200

    feat: add 02, 03 tasks

---
+++ 02-write-file/index.js
@@ -0,0 +1,51 @@
+const fs = require('fs').promises;
+const readline = require('readline');
+const path = require('path');
+
+const filePath = path.join(__dirname, './output.txt');
+
+const farewellMessage = 'Thank for use program. Good by!';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+async function askForInput() {
+  try {
+    const input = await new Promise((resolve) => {
+      rl.question('Type text or "exit" for quit): ', resolve);
+    });
+
+    if (input.toLowerCase() === 'exit') {
+      console.log(farewellMessage);
+      rl.close();
+      process.exit();
+    }
+
+    await fs.appendFile(filePath, input + '\n', 'utf8');
+    console.log('The text is written to the file.');
+    askForInput();
+  } catch (err) {
+    console.error('An error occurred:', err);
+    rl.close();
+    process.exit(1);
+  }
+}
+
+(async () => {
+  try {
+    await fs.access(filePath, fs.constants.F_OK);
+  } catch (err) {
+    console.log('Create a new file:', filePath);
+    await fs.writeFile(filePath, '', 'utf8');
+  }
+
+  console.log('Hello! Enter the text.');
+  askForInput();
+})();
+
+rl.on('close', () => {
+  console.log(farewellMessage);
+  process.exit();
+});
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs/promises');
+const path = require('path');
+async function readFolderContent(folderPath) {
+  try {
+    const files = await fs.readdir(folderPath, { withFileTypes: true });
+    // console.log(`Список файлів у папці ${folderPath}:`);
+    for (const file of files) {
+      const filePath = path.join(folderPath, file.name);
+      const stats = await fs.stat(filePath);
+
+      if (file.isFile()) {
+        const fileName = path.parse(file.name).name;
+        const fileExtension = path.extname(file.name).slice(1);
+        const fileSize = stats.size;
+        console.log(`${fileName} - ${fileExtension} - ${fileSize} byte`);
+      }
+    }
+  } catch (err) {
+    console.error(`Помилка при читанні папки ${folderPath}:`, err);
+  }
+}
+const folderPath = path.join(__dirname, './secret-folder');
+readFolderContent(folderPath);
