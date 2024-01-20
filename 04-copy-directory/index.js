89fa01a691e1a9ce2ae1ce9e1e438d909e7f6466
Zelenskiyas <zelensk1973@gmail.com>
2024-01-20 16:44:11 +0200

    feat: add recursive in 04 tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,24 +1,23 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-async function copyDir() {
-  const sourceFolder = path.join(__dirname, 'files');
-  const destinationFolder = path.join(__dirname, 'files-copy');
-
+async function copyDir(sourceFolder, destinationFolder) {
   try {
-    // Create the destination folder if it does not exist
     await fs.mkdir(destinationFolder, { recursive: true });
 
-    // Read the contents of the source folder
     const files = await fs.readdir(sourceFolder);
 
-    // Copy each file from the source folder to the destination folder
     for (const file of files) {
       const sourceFilePath = path.join(sourceFolder, file);
       const destinationFilePath = path.join(destinationFolder, file);
 
-      // Use fs.copyFile to copy individual files
-      await fs.copyFile(sourceFilePath, destinationFilePath);
+      const fileStat = await fs.stat(sourceFilePath);
+
+      if (fileStat.isDirectory()) {
+        await copyDir(sourceFilePath, destinationFilePath);
+      } else {
+        await fs.copyFile(sourceFilePath, destinationFilePath);
+      }
     }
 
     console.log('Directory copied successfully!');
@@ -27,4 +26,7 @@ async function copyDir() {
   }
 }
 
-copyDir();
+const sourceFolder = path.join(__dirname, 'files');
+const destinationFolder = path.join(__dirname, 'files-copy');
+
+copyDir(sourceFolder, destinationFolder);
