cb10f5e30185b058ce669a2e05d91b583c3f39c8
Zelenskiyas <zelensk1973@gmail.com>
2024-01-24 18:25:15 +0200

    fix: remove files in task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,33 @@
 const fs = require('fs/promises');
 const path = require('path');
 
+async function removeDirContents(directory) {
+  const files = await fs.readdir(directory);
+
+  for (const file of files) {
+    const filePath = path.join(directory, file);
+    const fileStat = await fs.stat(filePath);
+
+    if (fileStat.isDirectory()) {
+      await removeDirContents(filePath);
+      await fs.rmdir(filePath);
+    } else {
+      await fs.unlink(filePath);
+    }
+  }
+}
+
 async function copyDir(sourceFolder, destinationFolder) {
   try {
+
+    const destinationFolderExists = await fs.access(destinationFolder)
+      .then(() => true)
+      .catch(() => false);
+
+    if (destinationFolderExists) {
+      await removeDirContents(destinationFolder);
+    }
+
     await fs.mkdir(destinationFolder, { recursive: true });
 
     const files = await fs.readdir(sourceFolder);
